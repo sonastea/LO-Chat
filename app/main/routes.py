b4542b639c50c124a89bf6d405eb51b37b72be0d
Johnny Thavisay <sonastea@gmail.com>
2021-01-26 22:11:03 -0800

    Add sign in form with handling of data with errors

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,16 +1,19 @@
 from .forms import LoginForm
 from app.main import bp
-from flask import render_template
+from flask import render_template, flash, redirect
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
-    return render_template('index.html', title='Home', form=form)
+    return render_template('index.html', title='Home')
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me {}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Login', form=form)

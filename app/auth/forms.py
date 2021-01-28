4ee79f0c091935f51bbf2c5e1c308b2c6ceeda99
Johnny Thavisay <sonastea@gmail.com>
2021-01-28 15:17:10 -0800

    Moved files around to be easier for expandability Added user register and logout functions

---
+++ app/auth/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from app.models import User
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    """User login form template"""
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    """User registration form template"""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """Checks if the username is not taken"""
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+
+    def validate_email(self, email):
+        """Checks if the email is not taken"""
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,19 +1,10 @@
-from .forms import LoginForm
+from flask import render_template, redirect, url_for
+from flask_login import current_user, login_required
 from app.main import bp
-from flask import render_template, flash, redirect
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     return render_template('index.html', title='Home')
-
-
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me {}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Login', form=form)
